4afb983df9bb5fee1c48402b63462539504b6036
jwkeena <jwkeena@gmail.com>
2019-08-11 17:09:57 -0400

    Back to image onload progress bar

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 // Hide site until loaded adapted from 
 // https://www.amberweinberg.com/using-js-to-hide-site-until-loaded/ && https://www.utopiamechanicus.com/article/javascript-wait-for-img-load/
+
 function processImages()  
 {
-  if ( loadingFlags[0] || loadingFlags[1])
+  if ( loadingFlags[0])
   {
     // check often until loaded
     window.setTimeout(processImages,50);
@@ -10,16 +11,11 @@ function processImages()
   }
     $("main").addClass("all-loaded");
     $(".progress").css("display", "none");
-};
+}
 
-if ($.browser.mozilla) {
-    $("main").addClass("all-loaded");
-    $(".progress").css("display", "none");
-} else {
-    $(document).ready(function () {
-        window.setTimeout(processImages,1000);
-    })
-};
+$(document).ready(function () {
+    window.setTimeout(processImages,1000);
+})
 
 // Materialize scrollspy http://www.freetimelearning.com/materialize-css/example-codes/scroll-spy.html
 $(document).ready(function () {
